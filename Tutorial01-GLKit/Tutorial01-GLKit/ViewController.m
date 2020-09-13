2b6950975e8113c898cbc125760de79921e81472
zhanlong <zhanlong@mail.hudongco.com>
2020-09-13 10:58:22 +0800

    upload Tutorial01

--- Tutorial01-GLKit/Tutorial01-GLKit/ViewController.m
+++ Tutorial01-GLKit/Tutorial01-GLKit/ViewController.m
@@ -23,11 +23,12 @@
     
     [self setupConfig];
     [self uploadVertexArray];
-    [self updateTexture];
+    [self uploadTexture];
+    
 }
 
 - (void)setupConfig {
-    // 创建OpenGL ES 上下文
+    
     self.mContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
     GLKView *view = (GLKView *)self.view;
     view.context = self.mContext;
@@ -37,8 +38,8 @@
 }
 
 - (void)uploadVertexArray {
-    // 设置顶点数据 前三个是顶点坐标（x、y、z轴），后面两个是纹理坐标（x，y）
-    GLfloat vertextData[] =
+    
+    GLfloat vertexData[] =
     {
         0.5, -0.5, 0.0f,    1.0f, 0.0f, //右下
         0.5, 0.5, -0.0f,    1.0f, 1.0f, //右上
@@ -49,24 +50,23 @@
         -0.5, -0.5, 0.0f,   0.0f, 0.0f, //左下
     };
     
-    // 顶点数据缓存
     GLuint buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertextData), vertextData, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
     
     glEnableVertexAttribArray(GLKVertexAttribPosition);
-    glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat)*5, NULL);
+    glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat)*5, (GLfloat*)NULL+0);
     
     glEnableVertexAttribArray(GLKVertexAttribTexCoord0);
     glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_FALSE, sizeof(GLfloat)*5, (GLfloat*)NULL+3);
     
 }
 
-- (void)updateTexture {
-    // 设置纹理贴图
+- (void)uploadTexture {
+    
     NSString *filePath = [[NSBundle mainBundle] pathForResource:@"for_test" ofType:@"jpg"];
-    NSDictionary *options = @{GLKTextureLoaderOriginBottomLeft:@YES};
+    NSDictionary *options = @{GLKTextureLoaderOriginBottomLeft:@(1)};
     GLKTextureInfo *textureInfo = [GLKTextureLoader textureWithContentsOfFile:filePath options:options error:nil];
     
     self.mEffect = [[GLKBaseEffect alloc] init];
@@ -76,11 +76,10 @@
 }
 
 - (void)glkView:(GLKView *)view drawInRect:(CGRect)rect {
-    // 设置渲染场景
-    glClearColor(0.3f, 0.6f, 1.0f, 1.0f);
+    
+    glClearColor(0.3, 0.6, 1.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
-    // 启动着色器
     [self.mEffect prepareToDraw];
     glDrawArrays(GL_TRIANGLES, 0, 6);
 }
